9d4f9a0750683443fc80be2c9713ef2cd8d7a65a
Jacob Froman <jacob.h.froman@gmail.com>
2016-11-29 14:44:16 -0600

    test button click in jQuery

--- scripts/garage.js
+++ scripts/garage.js
@@ -1,1 +1,8 @@
 console.log('testify');
+
+$(document).ready(function() {
+  console.log('jQ ready freddy');
+  $('#testButton').on('click', function() {
+    console.log('button clicked');
+  });
+});
